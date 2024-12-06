68f799fad73a7995f50f64aa3f4dcc49d208a588
Ганжаргал Гантөмөр <24LP0756@G401-CC0110.local>
2024-12-06 17:32:04 +0800

    ganjaa

---
+++ web/components/features/homepage/Hero.tsx
@@ -0,0 +1,22 @@
+export const Hero = () => {
+  return (
+    <div
+      className="w-full h-[654px] "
+      style={{
+        backgroundImage: "url(/mongolia.jpeg)",
+        backgroundSize: "cover",
+        backgroundPosition: "end",
+      }}
+    >
+      <div>
+        <div className="w-[590px] p-10">
+          <p className="text-[72px]">Buy, sell, and showcase NFTs</p>
+          <p className="text-[20px]">
+            Create, Explore, & Collect Digital Art NFTs.
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
