b5578afef7ba1c8e6032495909913cb7bf9c0adb
ligthman <jamka888@gmail.com>
2024-12-19 16:01:04 +0800

    order done

--- web/components/pages/CategoryPage.tsx
+++ web/components/pages/CategoryPage.tsx
@@ -1,31 +1,57 @@
 "use client";
-import MainCard from "@/components/ui/cards/MainCard";
 import { BACKEND_ENDPOINT } from "@/constant/mockdatas";
 import { TPlaces } from "@/types/DataTypes";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Places } from "../features/categorysinglepage/Places";
 import { CategoryMap } from "../features/categorysinglepage/CategoryMap";
+import Link from "next/link";
+import { DistributorCard } from "../ui/cards/DistributorCard";
 
 export default function CategoryPage() {
   const [data, setData] = useState<TPlaces[]>([]);
+  const [loader, setLoader] = useState<boolean>(false);
   const params = useParams();
   const categoryName = params.categorysearch;
 
   const fetchdata = async () => {
+    setLoader(true);
     try {
       const response = await fetch(
         `${BACKEND_ENDPOINT}/api/selected?categorizedPlaces=${categoryName}`
       );
       const result = await response.json();
       setData(result.data);
+      setLoader(false);
     } catch (error) {
+      setLoader(false);
       throw new Error();
+    } finally {
+      setLoader(false);
     }
   };
   useEffect(() => {
     fetchdata();
   }, []);
+
+  if (loader) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <p>Уншиж байна...</p>
+      </div>
+    );
+  }
+
+  if (data.length == 0) {
+    return (
+      <div className="w-screen h-screen flex flex-col items-center justify-center gap-4">
+        <div>Хайлтын илэрц байхгүй байна.</div>
+        <Link href={"/menu"}>
+          <DistributorCard name="Буцах" id="" />
+        </Link>
+      </div>
+    );
+  }
   return (
     <main className="w-screen flex  justify-center">
       <div className="container grid grid-cols-2 gap-4">
--- web/components/pages/searchedPage.tsx
+++ web/components/pages/searchedPage.tsx
@@ -9,12 +9,15 @@ import Link from "next/link";
 export const SearchedPage = () => {
   const searchParams = useSearchParams();
   const [searchedData, setSearchedData] = useState<TPlaces[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   // ///////////////////////////
   const location = searchParams.get("location");
   const category = searchParams.get("category");
   const peoplecount = searchParams.get("peoplecount");
   // ////////////////////////////
+
   const SearchedDataFetch = async () => {
+    setLoading(true);
     try {
       const response = await fetch(
         `${BACKEND_ENDPOINT}/api/selected?placesLocation=${location}&categorizedPlaces=${category}&capacity=${peoplecount}`
@@ -22,17 +25,28 @@ export const SearchedPage = () => {
       const result = await response.json();
       if (result.success == true) {
         setSearchedData(result.data);
+        setLoading(false);
       } else {
         return searchedData;
       }
     } catch (error) {
+      setLoading(false);
       throw new Error();
+    } finally {
+      setLoading(false);
     }
   };
   //////////////////////////////
   useEffect(() => {
     SearchedDataFetch();
   }, []);
+  if (loading) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <p>Уншиж байна...</p>
+      </div>
+    );
+  }
   if (searchedData.length == 0) {
     return (
       <div className="w-screen h-screen flex flex-col justify-center items-center gap-4 text-[#333]">
@@ -46,25 +60,29 @@ export const SearchedPage = () => {
       </div>
     );
   }
+
   return (
-    <main className="w-screen min-h-screen flex">
-      {searchedData &&
-        searchedData.map((data: TPlaces) => {
-          return (
-            <div key={data._id + "searched"}>
-              <MainCard
-                _id={data?._id}
-                name={data?.name}
-                description={data?.description}
-                capacity={data?.capacity}
-                location={data?.location}
-                image={data?.image}
-                category={data?.category}
-                workingHours={data?.workingHours}
-              />
-            </div>
-          );
-        })}
+    <main className="w-screen min-h-screen flex pt-[65px]">
+      <div className="mx-auto container">
+        {searchedData &&
+          searchedData.map((data: TPlaces) => {
+            return (
+              <div key={data._id + "searched"}>
+                <MainCard
+                  _id={data?._id}
+                  name={data?.name}
+                  description={data?.description}
+                  capacity={data?.capacity}
+                  phoneNumber={data.phoneNumber}
+                  location={data?.location}
+                  image={data?.image}
+                  category={data?.category}
+                  workingHours={data?.workingHours}
+                />
+              </div>
+            );
+          })}
+      </div>
     </main>
   );
 };
